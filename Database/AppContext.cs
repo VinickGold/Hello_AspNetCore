c795496b05c4fc752c6c327771c626e9b9cb3d91
Vinicius Goldenberg <vinicius.goldenberg@outlook.com>
2017-09-17 21:47:04 -0300

    Configure Entity Framework Core

---
+++ Database/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Database
+{
+	public class MyAppContext : DbContext
+	{
+		public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+	}
+}
---
+++ Database/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Database
+{
+    public static class DbInitializer
+    {
+		public static void Initialize(IServiceCollection services , string connectionString) 
+		{
+			services.AddDbContext<MyAppContext>(options => options.UseSqlServer(connectionString));
+		}
+    }
+}
